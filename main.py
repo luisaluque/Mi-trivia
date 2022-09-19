bb81ac65c9a23ce126b95c4d396cdff8426dc295
Luisa Luque <>
2022-09-19 04:25:49 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,23 @@
+# Lo primero es mostrar en pantalla el texto de bienvenida para quien juegue tu trivia
+print ("Bienvenido a mi trivia sobre computación")
+print ("Pondremos a prueba tus conocimientos")
+
+# Es importante dar instrucciones sobre cómo jugar:
+print ("Responder las siguientes preguntas escribiendo la letra de la alternativa y presionando 'Entrer' para enviar tu respuesta:\n")
+
+# OJO, el \n al final de la línea 6 es para dar un "salto de línea"
+
+# Pregunta 1
+print ("1) ¿cuales son las células sanguineas?")
+print ("a) plaquetas y hepatocitos ")
+print ("b) eritrocitos y leucocitos ")
+print ("c) neurona y eritrocitos")
+print ("d) linfocitos y ovulos")
+
+# Almacenamos la respuesta del usuario en la variable "respuesta_1"
+respuesta_1 = input("\nTu respuesta: b")
+
+# pregunta 2
+print ("2) ¿ cuales son los tipos de neutrofilos?") 
+print ("a) abastonados")
+print ("b) segmentados")
